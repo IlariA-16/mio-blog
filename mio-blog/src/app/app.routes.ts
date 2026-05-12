c42d8896d55be501516dab4a185a9ba5aef53f9d
IlariA-16 <ilariataddei@hotmail.com>
2026-05-12 17:49:10 +0200

    Creazione blog personale

---
+++ mio-blog/src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/home/home')
+        .then(m => m.Home)
+  },
+  {
+    path: 'blog',
+    loadComponent: () =>
+      import('./features/blog/blog')
+        .then(m => m.Blog)
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./features/about/about')
+        .then(m => m.About)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
