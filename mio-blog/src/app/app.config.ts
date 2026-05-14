0f50f07fb68e07b743983a07d785a8b570c3df08
IlariA-16 <ilariataddei@hotmail.com>
2026-05-14 17:53:28 +0200

    installato il linguaggio e creata cartella per linguaggi json

--- mio-blog/src/app/app.config.ts
+++ mio-blog/src/app/app.config.ts
@@ -1,10 +1,29 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// Funzione factory per indicare ad Angular dove trovare i file JSON
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withFetch()), // Permette ad Angular di leggere i file JSON locali
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        defaultLanguage: 'it', // Imposta l'italiano come lingua di avvio predefinita
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+      })
+    )
   ]
 };
