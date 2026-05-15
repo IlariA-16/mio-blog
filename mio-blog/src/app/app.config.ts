6b75fbd32fb53ac8cff90efc1722d6c5b9a6887a
IlariA-16 <ilariataddei@hotmail.com>
2026-05-15 17:21:57 +0200

    fix: risolti problemi di routing e traduzioni per produzione

--- mio-blog/src/app/app.config.ts
+++ mio-blog/src/app/app.config.ts
@@ -1,11 +1,10 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router'; // Aggiunto conHashLocation
 import { routes } from './app.routes';
 import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader, TRANSLATE_HTTP_LOADER_CONFIG } from '@ngx-translate/http-loader'; // Aggiungi l'import del token
+import { TranslateHttpLoader, TRANSLATE_HTTP_LOADER_CONFIG } from '@ngx-translate/http-loader';
 
-// 1. La factory ora è semplicissima: niente parametri nel costruttore!
 export function HttpLoaderFactory(http: HttpClient) {
   return new (TranslateHttpLoader as any)(http);
 }
@@ -13,14 +12,13 @@ export function HttpLoaderFactory(http: HttpClient) {
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()), // Abilitato l'Hash Location Strategy per far funzionare le rotte online
     provideHttpClient(withFetch()), 
     
-    // 2. FORNIAMO NOI LA CONFIGURAZIONE CHE LUI CERCA
     {
       provide: TRANSLATE_HTTP_LOADER_CONFIG,
       useValue: {
-        prefix: '/i18n/',
+        prefix: './i18n/', // Modificato in './i18n/' con il punto per caricare le traduzioni su GitHub Pages
         suffix: '.json'
       }
     },
